119dbf3e6863fdfaeff3206cca716f7510ef4793
Ritika <ritika@codecolonies.com>
2026-05-12 18:58:12 +0530

    Data-types

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// index.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+const greeting = "Hello, GeeksforGeeks!";
+console.log(greeting);
+//# sourceMappingURL=index.js.map
